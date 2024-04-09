97800ba4f1281c2896271293bb7178fc157fc41b
sanghee0820 <lsh277604@gmail.com>
2024-04-09 13:23:15 +0900

    project initialization

---
+++ src/main/java/com/finnect/FinnectApplication.java
@@ -0,0 +1,13 @@
+package com.finnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finnect/FinnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ea9e2a43cc19afcb1162418421957a87cab0e371
sanghee0820 <lsh277604@gmail.com>
2024-04-20 13:35:39 +0900

    feat: 매핑 추가 및 생성자 추가

--- src/main/java/com/finnect/deal/domain/Deal.java
+++ src/main/java/com/finnect/deal/domain/Deal.java
@@ -2,31 +2,51 @@ package com.finnect.deal.domain;
 
 import com.finnect.mockDomain.Company;
 import com.finnect.mockDomain.Member;
-import com.finnect.mockDomain.Row;
+import com.finnect.mockDomain.DataRow;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
 
 
-
-@Getter
-@Builder
 @AllArgsConstructor
+@Builder
+@Entity
 public class Deal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long dealId;
 
     @Setter
+    @Getter
     private String dealName;
 
 
     // Identified By Company
     // Mock Entity
+    @OneToOne(fetch = FetchType.EAGER)
+    @Getter
     private Company company;
     // Identified By userId & workspaceId
     // Mock Entity
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @Getter
     private Member member;
     // Identified By rowId
     // Mock Entity
-    private Row row;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @Getter
+    private DataRow dataRow;
+
+    protected Deal() {}
 }

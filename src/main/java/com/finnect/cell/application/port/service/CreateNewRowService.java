1df904aea284e762f1db574d349d8df413b036ee
sanghee0820 <lsh277604@gmail.com>
2024-04-28 00:47:52 +0900

    refactor: Column -> DataColumn 이름 변경

--- src/main/java/com/finnect/cell/application/port/service/CreateNewRowService.java
+++ src/main/java/com/finnect/cell/application/port/service/CreateNewRowService.java
@@ -2,8 +2,8 @@ package com.finnect.cell.application.port.service;
 
 import com.finnect.cell.application.port.in.CreateNewRowUseCase;
 import com.finnect.cell.application.port.out.SaveDataRowPort;
-import com.finnect.cell.application.port.out.SaveNewCellUsePort;
-import com.finnect.cell.domain.Column;
+import com.finnect.cell.application.port.out.SaveCellPort;
+import com.finnect.cell.domain.DataColumn;
 import com.finnect.cell.domain.DataRow;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +13,11 @@ import org.springframework.stereotype.Service;
 public class CreateNewRowService implements CreateNewRowUseCase {
 
     private final SaveDataRowPort saveDataRowPort;
-    private final SaveNewCellUsePort saveNewCellUsePort;
+    private final SaveCellPort saveCellPort;
     @Override
-    public DataRow createNewDataRow(Column column) {
-        DataRow dataRow = saveDataRowPort.createNewDataRow();
-        saveNewCellUsePort.saveNewCell(column, dataRow);
+    public DataRow createNewDataRow(DataColumn dataColumn) {
+        DataRow dataRow = saveDataRowPort.saveNewDataRow();
+        saveCellPort.saveNewCellByNewRow(dataColumn, dataRow);
         return dataRow;
     }
 }

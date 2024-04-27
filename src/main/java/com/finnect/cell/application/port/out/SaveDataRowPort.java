dc9e3beb71bb26b851c46576622ec04f269ea989
sanghee0820 <lsh277604@gmail.com>
2024-04-28 00:46:35 +0900

    feat: 새로운 Column 생성 포트 추가

--- src/main/java/com/finnect/cell/application/port/out/SaveDataRowPort.java
+++ src/main/java/com/finnect/cell/application/port/out/SaveDataRowPort.java
@@ -1,6 +1,7 @@
 package com.finnect.cell.application.port.out;
 
 import com.finnect.cell.domain.DataRow;
+import com.finnect.cell.domain.state.DataColumnState;
 
 public interface SaveDataRowPort {
 

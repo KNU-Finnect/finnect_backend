f12a202dbac1a088783dc00a86f06261bed5786f
sanghee0820 <lsh277604@gmail.com>
2024-04-28 00:48:25 +0900

    feat: 새 Column 저장 기능

---
+++ src/main/java/com/finnect/cell/application/port/service/CreateNewColumnService.java
@@ -0,0 +1,25 @@
+package com.finnect.cell.application.port.service;
+
+import com.finnect.cell.application.port.in.CreateNewColumnUseCase;
+import com.finnect.cell.application.port.out.SaveCellPort;
+import com.finnect.cell.application.port.out.SaveDataColumnPort;
+import com.finnect.cell.domain.DataColumn;
+import com.finnect.cell.domain.state.DataColumnState;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CreateNewColumnService implements CreateNewColumnUseCase {
+    private final SaveCellPort saveCellPort;
+    private final SaveDataColumnPort saveDataColumnPort;
+    @Override
+    public DataColumnState createNewColumn(DataColumn dataColumn) {
+        log.info(dataColumn.toString());
+        dataColumn = saveDataColumnPort.saveNewColumn(dataColumn);
+        saveCellPort.saveNewCellByNewColumn(dataColumn);
+        return null;
+    }
+}

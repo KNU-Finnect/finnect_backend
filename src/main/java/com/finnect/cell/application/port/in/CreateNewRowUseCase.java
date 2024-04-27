ed604846378a22c3a3b6de8946df3a81c226960b
sanghee0820 <lsh277604@gmail.com>
2024-04-28 00:46:19 +0900

    feat: 새로운 Column 생성 유즈케이스 추가

--- src/main/java/com/finnect/cell/application/port/in/CreateNewRowUseCase.java
+++ src/main/java/com/finnect/cell/application/port/in/CreateNewRowUseCase.java
@@ -1,9 +1,8 @@
 package com.finnect.cell.application.port.in;
 
-import com.finnect.cell.domain.Column;
+import com.finnect.cell.domain.DataColumn;
 import com.finnect.cell.domain.DataRow;
 
 public interface CreateNewRowUseCase {
-    DataRow createNewDataRow(Column column);
-
+    DataRow createNewDataRow(DataColumn dataColumn);
 }

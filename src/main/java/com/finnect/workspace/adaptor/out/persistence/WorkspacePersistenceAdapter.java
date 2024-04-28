3b30c06d56f84f06bac09c7f3d4e2749faf43c10
HyeonJeong <wgwjh05169@gmail.com>
2024-04-29 02:26:47 +0900

    feat: 영속성 어댑터와 모델에 워크스페이스 이름 변경 메소드 추가

--- src/main/java/com/finnect/workspace/adaptor/out/persistence/WorkspacePersistenceAdapter.java
+++ src/main/java/com/finnect/workspace/adaptor/out/persistence/WorkspacePersistenceAdapter.java
@@ -2,12 +2,15 @@ package com.finnect.workspace.adaptor.out.persistence;
 
 import com.finnect.workspace.WorkspaceState;
 import com.finnect.workspace.application.port.out.CreateWorkspacePort;
+import com.finnect.workspace.application.port.out.UpdateWorkspacePort;
+import com.finnect.workspace.error.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 class WorkspacePersistenceAdapter implements
-        CreateWorkspacePort {
+        CreateWorkspacePort, UpdateWorkspacePort {
     private final WorkspaceRepository workspaceRepository;
 
     @Autowired
@@ -24,5 +27,16 @@ class WorkspacePersistenceAdapter implements
         WorkspaceState savedState = workspaceRepository.save(workspaceJpaEntity);
 
         return savedState;
+    }
+
+    @Transactional
+    @Override
+    public WorkspaceState updateWorkspace(WorkspaceState state) {
+        WorkspaceJpaEntity workspaceJpaEntity = workspaceRepository.findById(state.getWorkspaceId())
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 워크스페이스입니다."));
+
+        workspaceJpaEntity.updateWorkspace(state.getWorkspaceName());
+
+        return workspaceJpaEntity;
     }
 }

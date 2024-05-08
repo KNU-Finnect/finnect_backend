aa819f74b4e45dc3ce49c23530e88ba62d1ac386
HyeonJeong <wgwjh05169@gmail.com>
2024-05-08 13:46:22 +0900

    feat: 멤버 조회 웹 어댑터 구현

--- src/main/java/com/finnect/workspace/adaptor/in/web/MemberController.java
+++ src/main/java/com/finnect/workspace/adaptor/in/web/MemberController.java
@@ -2,23 +2,25 @@ package com.finnect.workspace.adaptor.in.web;
 
 import com.finnect.common.Response;
 import com.finnect.workspace.MemberState;
-import com.finnect.workspace.WorkspaceState;
 import com.finnect.workspace.adaptor.in.web.req.CreateMemberRequest;
-import com.finnect.workspace.adaptor.in.web.req.CreateWorkspaceRequest;
 import com.finnect.workspace.adaptor.in.web.res.CreateMemberResponse;
-import com.finnect.workspace.adaptor.in.web.res.CreateWorkspaceResponse;
+import com.finnect.workspace.adaptor.in.web.res.dto.FindMembersResponse;
 import com.finnect.workspace.adaptor.in.web.res.dto.MemberDto;
-import com.finnect.workspace.adaptor.in.web.res.dto.WorkspaceDto;
+import com.finnect.workspace.adaptor.in.web.res.dto.MemberWithoutIdDto;
 import com.finnect.workspace.application.port.in.CreateMemberCommand;
 import com.finnect.workspace.application.port.in.CreateMemberUsecase;
-import com.finnect.workspace.application.port.in.CreateWorkspaceCommand;
+import com.finnect.workspace.application.port.in.FindMembersQuery;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final CreateMemberUsecase createMemberUsecase;
+    private final FindMembersQuery findMembersQuery;
 
     @PostMapping("/workspaces/members")
     public ResponseEntity<Response<CreateMemberResponse>> createWorkspace(@RequestBody CreateMemberRequest request) {
@@ -47,5 +50,17 @@ public class MemberController {
         );
         CreateMemberResponse createMemberResponse = new CreateMemberResponse(memberDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(new Response(HttpStatus.OK.value(), createMemberResponse));
+    }
+
+    @GetMapping("/workspaces/members")
+    public ResponseEntity<Response> findMembers() {
+        Long workspaceId = 1L;
+
+        List<MemberWithoutIdDto> members = findMembersQuery.loadMembersByWorkspace(workspaceId)
+                .stream()
+                .map(MemberWithoutIdDto::new)
+                .collect(Collectors.toList());
+        FindMembersResponse findMembersResponse = new FindMembersResponse(members);
+        return ResponseEntity.status(HttpStatus.OK).body(new Response(HttpStatus.OK.value(), findMembersResponse));
     }
 }

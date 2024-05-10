a20c8d271803633e092cbe0002c30ae1dd121dac
HyeonJeong <wgwjh05169@gmail.com>
2024-05-10 19:08:31 +0900

    feat: 멤버가 없는 워크스페이스 삭제

--- src/main/java/com/finnect/workspace/application/ExitWorkspaceService.java
+++ src/main/java/com/finnect/workspace/application/ExitWorkspaceService.java
@@ -2,7 +2,9 @@ package com.finnect.workspace.application;
 
 import com.finnect.workspace.MemberState;
 import com.finnect.workspace.application.port.in.ExitWorkspaceUsecase;
+import com.finnect.workspace.application.port.out.DeleteWorkspacePort;
 import com.finnect.workspace.application.port.out.ExitWorkspacePort;
+import com.finnect.workspace.application.port.out.FindMembersPort;
 import com.finnect.workspace.application.port.out.GetMemberPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,18 @@ import org.springframework.stereotype.Component;
 public class ExitWorkspaceService implements ExitWorkspaceUsecase {
     private final GetMemberPort getMemberPort;
     private final ExitWorkspacePort exitWorkspacePort;
+    private final FindMembersPort findMembersPort;
+    private final DeleteWorkspacePort deleteWorkspacePort;
 
     @Override
     public boolean exit(Long userId, Long workspaceId) {
         MemberState member = getMemberPort.getMember(userId, workspaceId);
 
         exitWorkspacePort.exit(member);
+
+        if (findMembersPort.findMembersByWorkspace(workspaceId).isEmpty()) {
+            deleteWorkspacePort.delete(workspaceId);
+        }
 
         return true;
     }

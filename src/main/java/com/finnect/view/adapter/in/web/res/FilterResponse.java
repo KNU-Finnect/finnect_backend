c571c29468087aa4addafec7f119350aed398c9f
sanghee0820 <lsh277604@gmail.com>
2024-05-11 23:48:28 +0900

    feat: View생성 Response 추가

---
+++ src/main/java/com/finnect/view/adapter/in/web/res/FilterResponse.java
@@ -0,0 +1,17 @@
+package com.finnect.view.adapter.in.web.res;
+
+import com.finnect.view.domain.constant.FilterCondition;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class FilterResponse {
+    private Long filterId;
+    private Long viewId;
+    private Long columnId;
+    private FilterCondition condition;
+    private String value;
+}

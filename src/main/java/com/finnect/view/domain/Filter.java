04edb121c91898942e5b068ef2d73ef7cb046c92
sanghee0820 <lsh277604@gmail.com>
2024-05-11 23:44:20 +0900

    feat: View 관련 도메인 엔티티 추가

---
+++ src/main/java/com/finnect/view/domain/Filter.java
@@ -0,0 +1,47 @@
+package com.finnect.view.domain;
+
+import com.finnect.view.domain.constant.FilterCondition;
+import lombok.Builder;
+
+public class Filter implements com.finnect.view.domain.state.FilterState {
+
+    private Long id;
+    private Long viewId;
+    private Long columnId;
+    private FilterCondition filterCondition;
+    private String value;
+
+    @Builder
+    public Filter(Long id, Long viewId, Long columnId, FilterCondition filterCondition, String value) {
+        this.id = id;
+        this.viewId = viewId;
+        this.columnId = columnId;
+        this.filterCondition = filterCondition;
+        this.value = value;
+    }
+
+    @Override
+    public Long getFilterId() {
+        return this.id;
+    }
+
+    @Override
+    public Long getViewId() {
+        return this.viewId;
+    }
+
+    @Override
+    public Long getColumnId() {
+        return this.columnId;
+    }
+
+    @Override
+    public FilterCondition getFilterCondition() {
+        return this.filterCondition;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+}
---
+++ src/main/java/com/finnect/view/domain/View.java
@@ -0,0 +1,61 @@
+package com.finnect.view.domain;
+
+import com.finnect.view.domain.state.FilterState;
+import com.finnect.view.domain.state.ViewColumnState;
+import com.finnect.view.domain.state.ViewState;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Builder;
+import lombok.ToString;
+
+@ToString
+public class View implements ViewState {
+    private Long viewId;
+    //FK
+    private Long workspaceId;
+    private String viewName;
+    private Boolean isMain;
+    private List<Filter> filters;
+    private List<ViewColumn> viewColumns;
+
+    @Builder
+    public View(Long viewId, Long workspaceId, String viewName, Boolean isMain, List<Filter> filters,
+                List<ViewColumn> viewColumns) {
+        this.viewId = viewId;
+        this.workspaceId = workspaceId;
+        this.viewName = viewName;
+        this.isMain = isMain;
+        this.filters = filters;
+        this.viewColumns = viewColumns;
+    }
+
+    @Override
+    public Long getViewId() {
+        return this.viewId;
+    }
+
+    @Override
+    public Long getWorkspaceId() {
+        return this.workspaceId;
+    }
+
+    @Override
+    public String getViewName() {
+        return this.viewName;
+    }
+
+    @Override
+    public Boolean isMain() {
+        return this.isMain;
+    }
+
+    @Override
+    public List<FilterState> getFilters() {
+        return new ArrayList<>(filters);
+    }
+
+    @Override
+    public List<ViewColumnState> getViewColumns() {
+        return new ArrayList<>(viewColumns);
+    }
+}

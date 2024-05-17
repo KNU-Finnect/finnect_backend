d2028b18c06111b5275f537c2b04f9def6978f17
HyeonJeong <wgwjh05169@gmail.com>
2024-05-17 21:27:41 +0900

    feat: Company 영속성 모델 구현

--- src/main/java/com/finnect/crm/adapter/out/persistence/company/CompanyEntity.java
+++ src/main/java/com/finnect/crm/adapter/out/persistence/company/CompanyEntity.java
@@ -1,25 +1,37 @@
 package com.finnect.crm.adapter.out.persistence.company;
 
 import com.finnect.crm.adapter.out.persistence.cell.DataRowEntity;
+import com.finnect.crm.domain.company.CompanyState;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "company")
-class CompanyEntity {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+class CompanyEntity implements CompanyState {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
     private Long companyId;
 
+    private Long workspaceId;
 
     @OneToOne(fetch = FetchType.EAGER)
-    @Getter
     private DataRowEntity dataRowEntity;
 
+    private String domain;
+
+    private String companyName;
+
+    @Override
+    public Long getDataRowId() {
+        return this.dataRowEntity.getDataRowId();
+    }
 }

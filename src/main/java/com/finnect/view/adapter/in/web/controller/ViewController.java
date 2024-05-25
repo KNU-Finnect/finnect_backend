f6a792491ce01db364b31008401dde2ac6a3d92f
sanghee0820 <lsh277604@gmail.com>
2024-05-25 18:00:46 +0900

    feat: View의 조건에 맞는 Cell 조회 API 추가

--- src/main/java/com/finnect/view/adapter/in/web/controller/ViewController.java
+++ src/main/java/com/finnect/view/adapter/in/web/controller/ViewController.java
@@ -2,36 +2,74 @@ package com.finnect.view.adapter.in.web.controller;
 
 import com.finnect.common.ApiUtils;
 import com.finnect.common.ApiUtils.ApiResult;
+import com.finnect.crm.application.port.in.cell.LoadDealWithCellUseCase;
 import com.finnect.crm.application.port.out.deal.LoadDealWithCellPort;
+import com.finnect.crm.domain.deal.Deal;
+import com.finnect.crm.domain.deal.DealCell;
 import com.finnect.view.adapter.in.web.req.CreateViewRequest;
+import com.finnect.view.adapter.in.web.req.FilterRequest;
 import com.finnect.view.adapter.in.web.res.CreateViewResponse;
-import com.finnect.view.adapter.out.SaveViewPort;
+import com.finnect.view.adapter.in.web.res.ViewInfoResponse;
 import com.finnect.view.application.port.in.CreateViewUseCase;
+import com.finnect.view.application.port.in.LoadViewUseCase;
+import com.finnect.view.domain.Filter;
+import com.finnect.view.domain.View;
+import com.finnect.view.domain.constant.FilterCondition;
 import com.finnect.view.domain.state.ViewState;
-import java.net.http.HttpResponse;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-
 @RequiredArgsConstructor
 public class ViewController {
-    private final LoadDealWithCellPort loadDealWithCellPort;
     private final CreateViewUseCase createViewUseCase;
+    private final LoadViewUseCase loadViewUseCase;
+    private final LoadDealWithCellUseCase loadDealWithCellUseCase;
     @PostMapping("/workspaces/views")
     public ResponseEntity<ApiResult<CreateViewResponse>> createView(@RequestBody CreateViewRequest request){
-//        loadDealWithCellPort.loadDealsWithCellsByFilter(null);
+
         log.info(request.toString());
         ViewState newView = createViewUseCase.createNewView(request.toDomain());
         return new ResponseEntity<>(
                 ApiUtils.success(HttpStatus.CREATED, new CreateViewResponse(newView)),
                 HttpStatus.CREATED);
+    }
+
+    @GetMapping("/workspaces/views/{viewId}")
+    public ResponseEntity<ApiResult<ViewInfoResponse>> getView(
+            @PathVariable Long viewId,
+            @RequestParam(required = false) List<FilterRequest> filters,
+            @RequestParam(required = true) int page
+    ){
+        List<Filter> filterList = (filters == null) ? Collections.emptyList() : filters.stream()
+                .map((filter) -> Filter.builder()
+                        .columnId(filter.getColumnId())
+                        .value(filter.getValue())
+                        .filterCondition(filter.getFilterCondition())
+                        .build())
+                .toList();
+        ViewState viewState = loadViewUseCase.loadViewInfo(View.builder()
+                        .viewId(viewId)
+                        .build(),
+                filterList);
+
+        log.info(viewState.toString());
+        List<DealCell> dealCells = loadDealWithCellUseCase.loadDealWithCell(1L, viewState.getFilters(), page);
+
+        return new ResponseEntity<>(ApiUtils.success(HttpStatus.OK, new ViewInfoResponse(viewState, dealCells))
+                , HttpStatus.OK);
     }
 
 }

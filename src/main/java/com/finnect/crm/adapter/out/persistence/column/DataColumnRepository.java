70700c7d6df42e1235a39f2493b739dbb6dc89a1
sanghee0820 <lsh277604@gmail.com>
2024-05-25 17:58:42 +0900

    feat: Column 개수 구하는 로직 추가

--- src/main/java/com/finnect/crm/adapter/out/persistence/column/DataColumnRepository.java
+++ src/main/java/com/finnect/crm/adapter/out/persistence/column/DataColumnRepository.java
@@ -1,11 +1,22 @@
 package com.finnect.crm.adapter.out.persistence.column;
 
+import com.finnect.crm.domain.column.DataType;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DataColumnRepository extends JpaRepository<DataColumnEntity, Long> {
     List<DataColumnEntity> findDataColumnEntitiesByWorkspaceId(Long workspaceId);
+
+    @Query(value = """
+        SELECT COUNT(dc)
+        FROM data_column dc
+        WHERE dc.workspaceId = :workspaceId AND dc.dType = :dataType
+    """)
+    int countDataColumnEntitiesByWorkspaceIdAndDType(@Param("workspaceId")Long workspaceId,
+                                                     @Param("dataType") DataType dataType);
 }

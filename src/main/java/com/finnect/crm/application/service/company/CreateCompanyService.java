6f83c761afacc5ac8289b159d1291a93c464fc10
HyeonJeong <wgwjh05169@gmail.com>
2024-05-25 21:03:42 +0900

    feat: 도메인 생성 서비스 구현

---
+++ src/main/java/com/finnect/crm/application/service/company/CreateCompanyService.java
@@ -0,0 +1,36 @@
+package com.finnect.crm.application.service.company;
+
+import com.finnect.crm.application.port.in.company.CreateCompanyCommand;
+import com.finnect.crm.application.port.in.company.CreateCompanyUsecase;
+import com.finnect.crm.application.port.out.company.LoadCompanyPort;
+import com.finnect.crm.application.port.out.company.SaveCompanyPort;
+import com.finnect.crm.domain.company.CompanyState;
+import com.finnect.crm.domain.company.CompanyWithoutId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Transactional
+class CreateCompanyService implements CreateCompanyUsecase {
+
+    private final LoadCompanyPort loadCompanyPort;
+    private final SaveCompanyPort saveCompanyPort;
+
+    @Override
+    public CompanyState createCompany(CreateCompanyCommand cmd) {
+        loadCompanyPort.loadByDomain(cmd.getDomain())
+                .ifPresent((loadedState) -> {
+                            throw new RuntimeException(loadedState.getDomain() + " 도메인이 이미 존재합니다.");
+                        });
+
+        CompanyWithoutId companyWithoutId = CompanyWithoutId.builder()
+                .domain(cmd.getDomain())
+                .companyName(cmd.getCompanyName())
+                .workspaceId(cmd.getWorkspaceId())
+                .build();
+
+        return saveCompanyPort.save(companyWithoutId);
+    }
+}

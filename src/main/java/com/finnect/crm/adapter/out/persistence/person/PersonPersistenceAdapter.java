f02b12c35793c6097d7654866672d85486965a42
HyeonJeong <wgwjh05169@gmail.com>
2024-05-26 20:57:10 +0900

    feat: Person load 영속성 어댑터 구현

--- src/main/java/com/finnect/crm/adapter/out/persistence/person/PersonPersistenceAdapter.java
+++ src/main/java/com/finnect/crm/adapter/out/persistence/person/PersonPersistenceAdapter.java
@@ -1,5 +1,6 @@
 package com.finnect.crm.adapter.out.persistence.person;
 
+import com.finnect.crm.application.port.out.person.LoadPersonPort;
 import com.finnect.crm.application.port.out.person.SavePersonPort;
 import com.finnect.crm.domain.person.PersonState;
 import lombok.RequiredArgsConstructor;
@@ -7,12 +8,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-class PersonPersistenceAdapter implements SavePersonPort {
+class PersonPersistenceAdapter implements SavePersonPort, LoadPersonPort {
 
     private final PersonRepository personRepository;
 
     @Override
     public PersonState save(PersonState state) {
         return personRepository.save(PersonEntity.from(state));
+    }
+
+    @Override
+    public PersonState loadById(Long personId) {
+        PersonEntity person = personRepository.findById(personId)
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 Person입니다."));
+        return person;
     }
 }

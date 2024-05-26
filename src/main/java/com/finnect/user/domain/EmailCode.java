d17f9611f2496ed651e36f166bc2016f430fd6fb
sinswitch <sin6708k@gmail.com>
2024-05-26 17:23:23 +0900

    feat: EmailCode 도메인 추가

---
+++ src/main/java/com/finnect/user/domain/EmailCode.java
@@ -0,0 +1,39 @@
+package com.finnect.user.domain;
+
+import com.finnect.user.state.EmailCodeState;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Builder @RequiredArgsConstructor
+public class EmailCode implements EmailCodeState {
+
+    @Getter
+    private final String email;
+
+    @Getter
+    private final Integer number;
+
+    @Getter
+    private final Long expirationSecond;
+
+    private Boolean verified;
+
+    @Override
+    public Boolean isVerified() {
+        return verified;
+    }
+
+    public void verify(Integer number) {
+        verified = this.number.equals(number);
+    }
+
+    public static EmailCode from(EmailCodeState emailCode) {
+        return EmailCode.builder()
+                .email(emailCode.getEmail())
+                .number(emailCode.getNumber())
+                .expirationSecond(emailCode.getExpirationSecond())
+                .verified(emailCode.isVerified())
+                .build();
+    }
+}

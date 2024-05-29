05d4b1eef307e0e02789231a7136d43481730cf3
sanghee0820 <lsh277604@gmail.com>
2024-05-29 23:21:32 +0900

    feat: columnType 변경 시 셀값 리셋 로직 추가

---
+++ src/main/java/com/finnect/crm/application/service/cell/ResetDataColumnService.java
@@ -0,0 +1,23 @@
+package com.finnect.crm.application.service.cell;
+
+import com.finnect.crm.application.port.in.cell.ResetDataCellUseCase;
+import com.finnect.crm.application.port.out.cell.LoadDataCellPort;
+import com.finnect.crm.application.port.out.cell.SaveCellPort;
+import com.finnect.crm.domain.cell.DataCell;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ResetDataColumnService implements ResetDataCellUseCase {
+    private final LoadDataCellPort loadDataCellPort;
+    private final SaveCellPort saveCellPort;
+    @Override
+    public void resetColumnInfoByModifyColumn(DataCell dataCell) {
+        List<DataCell> dataCells = loadDataCellPort.loadDataCellsByColumnId(dataCell);
+        dataCells.forEach(DataCell::resetValue);
+        saveCellPort.saveDataCells(new ArrayList<>(dataCells));
+    }
+}

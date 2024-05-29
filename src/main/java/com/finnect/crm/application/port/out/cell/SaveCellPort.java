ebb0574271563755f3caa341bdc0ab490b5e7443
sanghee0820 <lsh277604@gmail.com>
2024-05-29 23:20:51 +0900

    feat: Cell 저장 로직 추가

--- src/main/java/com/finnect/crm/application/port/out/cell/SaveCellPort.java
+++ src/main/java/com/finnect/crm/application/port/out/cell/SaveCellPort.java
@@ -1,11 +1,16 @@
 package com.finnect.crm.application.port.out.cell;
 
+import com.finnect.crm.domain.cell.state.DataCellState;
 import com.finnect.crm.domain.cell.state.DataColumnState;
 import com.finnect.crm.domain.cell.state.DataRowState;
+import java.util.List;
 
 public interface SaveCellPort {
 
     void saveNewCellByNewRow(DataColumnState column, DataRowState dataRow);
 
     void saveNewCellByNewColumn(DataColumnState column);
+
+
+    void saveDataCells(List<DataCellState> dataCells);
 }

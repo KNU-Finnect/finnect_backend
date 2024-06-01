e5cc22c6c88e7dc46f4ffa422d4afda0f73d026e
Sanghee0820 <lsh277604@gmail.com>
2024-06-01 12:05:47 +0900

    feat: workspaceId로 Company List 반환 로직 추가

--- src/main/java/com/finnect/crm/adapter/out/persistence/company/CompanyRepository.java
+++ src/main/java/com/finnect/crm/adapter/out/persistence/company/CompanyRepository.java
@@ -1,5 +1,6 @@
 package com.finnect.crm.adapter.out.persistence.company;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -7,4 +8,6 @@ import java.util.Optional;
 interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
 
      Optional<CompanyEntity> findByDomain(String domain);
+
+     List<CompanyEntity> findCompanyEntitiesByWorkspaceId(Long workspaceId);
 }

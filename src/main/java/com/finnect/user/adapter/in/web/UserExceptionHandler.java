58ba83ba76afc6d18ef521673fd64ee7b37d3bd8
sinswitch <sin6708k@gmail.com>
2024-06-01 19:45:22 +0900

    feat: NOT FOUND 처리 API 추가

---
+++ src/main/java/com/finnect/user/adapter/in/web/UserExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.finnect.user.adapter.in.web;
+
+import com.finnect.common.ApiUtils;
+import com.finnect.common.ApiUtils.ApiResult;
+import com.finnect.user.application.port.out.exception.EmailCodeNotFoundException;
+import com.finnect.user.application.port.out.exception.RefreshTokenNotFoundException;
+import com.finnect.user.application.port.out.exception.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class UserExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiResult<Object>> userNotFound(UserNotFoundException e) {
+        log.error(e.toString());
+
+        return ResponseEntity.ok(ApiUtils.fail(HttpStatus.NOT_FOUND));
+    }
+
+    @ExceptionHandler(RefreshTokenNotFoundException.class)
+    public ResponseEntity<ApiResult<Object>> refreshTokenNotFound(RefreshTokenNotFoundException e) {
+        log.error(e.toString());
+
+        return ResponseEntity.ok(ApiUtils.fail(HttpStatus.NOT_FOUND));
+    }
+
+    @ExceptionHandler(EmailCodeNotFoundException.class)
+    public ResponseEntity<ApiResult<Object>> emailCodeNotFound(EmailCodeNotFoundException e) {
+        log.error(e.toString());
+
+        return ResponseEntity.ok(ApiUtils.fail(HttpStatus.NOT_FOUND));
+    }
+}

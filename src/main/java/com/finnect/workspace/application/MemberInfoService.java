12b20381eaa78a495bf69223e79ea90e382dcbea
HyeonJeong <wgwjh05169@gmail.com>
2024-06-03 02:48:55 +0900

    feat: Member 업데이트 인커밍 포트 구현

--- src/main/java/com/finnect/workspace/application/MemberInfoService.java
+++ src/main/java/com/finnect/workspace/application/MemberInfoService.java
@@ -1,5 +1,8 @@
 package com.finnect.workspace.application;
 
+import com.finnect.workspace.application.port.in.UpdateMemberCommand;
+import com.finnect.workspace.application.port.in.UpdateMemberUsecase;
+import com.finnect.workspace.application.port.out.GetMemberPort;
 import com.finnect.workspace.domain.state.MemberState;
 import com.finnect.workspace.application.port.in.CreateMemberCommand;
 import com.finnect.workspace.application.port.in.CreateMemberUsecase;
@@ -12,9 +15,10 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Getter
-public class CreateMemberService implements CreateMemberUsecase {
+public class MemberInfoService implements CreateMemberUsecase, UpdateMemberUsecase {
 
     private final SaveMemberPort saveMemberPort;
+    private final GetMemberPort getMemberPort;
 
     @Override
     public MemberState createMember(CreateMemberCommand cmd) {
@@ -26,5 +30,14 @@ public class CreateMemberService implements CreateMemberUsecase {
         MemberState savedMember = saveMemberPort.saveMember(member);
 
         return savedMember;
+    }
+
+    @Override
+    public MemberState updateMember(UpdateMemberCommand command) {
+        Member member = Member.from(getMemberPort.getMember(command.getUserId(), command.getWorkspaceId()));
+
+        member.update(command.getNickname(), command.getRole(), command.getPhone());
+
+        return saveMemberPort.saveMember(member);
     }
 }

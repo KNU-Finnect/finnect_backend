772de919ac60629d1a6d4d2a4702e21e39f16e36
HyeonJeong <wgwjh05169@gmail.com>
2024-06-02 19:55:36 +0900

    refactor: 회사 생성에 access token 적용

--- src/main/java/com/finnect/crm/adapter/in/web/controller/company/CompanyController.java
+++ src/main/java/com/finnect/crm/adapter/in/web/controller/company/CompanyController.java
@@ -11,10 +11,12 @@ import com.finnect.crm.application.port.in.company.CreateCompanyUsecase;
 import com.finnect.crm.application.port.in.company.LoadCompanyUseCase;
 import com.finnect.crm.domain.company.CompanyState;
 import java.util.List;
+import com.finnect.user.vo.WorkspaceAuthority;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,11 +29,20 @@ public class CompanyController {
     private final CreateCompanyUsecase createCompanyUsecase;
     private final LoadCompanyUseCase loadCompanyUseCase;
 
+    @PreAuthorize("permitAll()")
     @PostMapping("/workspaces/companies")
     public ResponseEntity<ApiUtils.ApiResult<CreateCompanyResponse>> createCompany(@RequestBody CreateCompanyRequest request) {
+        Long workspaceId;
+        try {
+            workspaceId = WorkspaceAuthority.from(
+                    SecurityContextHolder.getContext().getAuthentication().getAuthorities()
+            ).workspaceId().value();
+        } catch (Exception e) {
+            throw new RuntimeException("워크스페이스 ID가 누락되었습니다.");
+        }
 
         CompanyState state = createCompanyUsecase.createCompany(
-                new CreateCompanyCommand(1L, request.getDomain(), request.getCompanyName())
+                new CreateCompanyCommand(workspaceId, request.getDomain(), request.getCompanyName())
         );
 
         return ResponseEntity.status(HttpStatus.OK).body(

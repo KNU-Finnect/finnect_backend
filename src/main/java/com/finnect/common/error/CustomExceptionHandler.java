bc09b40943f6b5006e63851e51ab5d09c6634989
HyeonJeong <wgwjh05169@gmail.com>
2024-06-02 20:24:53 +0900

    feat: Exception Handler 구현

---
+++ src/main/java/com/finnect/common/error/CustomExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.finnect.common.error;
+
+import com.finnect.common.ApiUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiUtils.ApiResult<String>> runtimeExceptionHandler(RuntimeException e) {
+        log.error(e.toString());
+
+        return ResponseEntity.ok(ApiUtils.fail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ApiUtils.ApiResult<String>> illegalStateExceptionHandler(IllegalStateException e) {
+        log.error(e.toString());
+
+        return ResponseEntity.ok(ApiUtils.fail(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiUtils.ApiResult<String>> notFoundExceptionHandler(NotFoundException e) {
+        log.error(e.toString());
+
+        return ResponseEntity.ok(ApiUtils.fail(HttpStatus.NOT_FOUND, e.getMessage()));
+    }
+}

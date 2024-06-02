aa24bd6d6a6340c46b24c23ddd91844c88db2634
sinswitch <sin6708k@gmail.com>
2024-06-02 19:55:43 +0900

    fix: 인증 Context 초기화 누락 픽스

--- src/main/java/com/finnect/user/adapter/in/security/AuthorizationFilter.java
+++ src/main/java/com/finnect/user/adapter/in/security/AuthorizationFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -37,6 +38,7 @@ public class AuthorizationFilter extends OncePerRequestFilter {
             authorizeUseCase.authorize(command);
         } else {
             logger.info("No bearer tokens");
+            SecurityContextHolder.clearContext();
         }
 
         filterChain.doFilter(request, response);

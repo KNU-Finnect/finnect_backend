8717c9299a76285abc716fb8f5a44e8e390df63b
HyeonJeong <wgwjh05169@gmail.com>
2024-06-04 22:58:57 +0900

    feat: 영속성 어댑터에 피플 조회 메소드 구현

--- src/main/java/com/finnect/crm/adapter/out/persistence/person/PersonRepository.java
+++ src/main/java/com/finnect/crm/adapter/out/persistence/person/PersonRepository.java
@@ -2,10 +2,22 @@ package com.finnect.crm.adapter.out.persistence.person;
 
 import com.finnect.crm.domain.person.PersonState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 interface PersonRepository extends JpaRepository<PersonEntity, Long> {
 
     List<PersonState> findAllByCompanyIdOrderByPersonName(Long companyId);
+
+    @Query("""
+        select p
+        from person p
+        where p.companyId in (
+            select c.companyId from company c where c.workspaceId = :wid)
+        order by p.personName
+        """
+    )
+    List<PersonState> findAllByWorkspaceId(@Param("wid") Long workspaceId);
 }

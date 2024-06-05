9c60c5728a028d5ac396049d65508950e57a24bd
sanghee0820 <lsh277604@gmail.com>
2024-06-05 20:50:07 +0900

    feat: workspace 생성 시 Default Column 영속성 저장 기능

--- src/main/java/com/finnect/crm/application/port/out/column/SaveDataColumnPort.java
+++ src/main/java/com/finnect/crm/application/port/out/column/SaveDataColumnPort.java
@@ -2,7 +2,10 @@ package com.finnect.crm.application.port.out.column;
 
 import com.finnect.crm.domain.column.DataColumn;
 import com.finnect.crm.domain.column.state.DataColumnState;
+import java.util.List;
 
 public interface SaveDataColumnPort {
     DataColumn saveColumn(DataColumnState column);
+
+    void saveColumns(List<DataColumnState> columns);
 }

5a50b1281a1c4a892661e9c12590eb94a73c0f73
HyeonJeong <wgwjh05169@gmail.com>
2024-06-05 23:43:50 +0900

    refactor: company 생성 시 data row도 생성되도록 수정

--- src/main/java/com/finnect/crm/application/service/company/CreateCompanyService.java
+++ src/main/java/com/finnect/crm/application/service/company/CreateCompanyService.java
@@ -1,9 +1,12 @@
 package com.finnect.crm.application.service.company;
 
+import com.finnect.crm.application.port.in.cell.CreateNewRowUseCase;
 import com.finnect.crm.application.port.in.company.CreateCompanyCommand;
 import com.finnect.crm.application.port.in.company.CreateCompanyUsecase;
 import com.finnect.crm.application.port.out.company.SaveCompanyPort;
 import com.finnect.crm.application.port.out.company.SearchCompanyPort;
+import com.finnect.crm.domain.cell.state.DataRowState;
+import com.finnect.crm.domain.column.DataColumn;
 import com.finnect.crm.domain.company.CompanyState;
 import com.finnect.crm.domain.company.CompanyWithoutId;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 class CreateCompanyService implements CreateCompanyUsecase {
 
     private final SearchCompanyPort searchCompanyPort;
+    private final CreateNewRowUseCase createNewRowUseCase;
     private final SaveCompanyPort saveCompanyPort;
 
     @Override
@@ -23,10 +27,17 @@ class CreateCompanyService implements CreateCompanyUsecase {
         if (searchCompanyPort.searchByDomain(cmd.getDomain()))
             throw new RuntimeException(cmd.getDomain() + " 도메인이 이미 존재합니다.");
 
+        DataRowState dataRow = createNewRowUseCase.createNewDataRow(
+                DataColumn.builder()
+                        .workspaceId(cmd.getWorkspaceId())
+                        .build()
+        );
+
         CompanyWithoutId companyWithoutId = CompanyWithoutId.builder()
                 .domain(cmd.getDomain())
                 .companyName(cmd.getCompanyName())
                 .workspaceId(cmd.getWorkspaceId())
+                .dataRowId(dataRow.getDataRowId())
                 .build();
 
         return saveCompanyPort.save(companyWithoutId);

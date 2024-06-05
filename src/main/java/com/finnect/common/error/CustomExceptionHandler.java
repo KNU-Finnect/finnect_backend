54bf706553efb7900172e8e0a59588338e428d8a
sinswitch <sin6708k@gmail.com>
2024-06-05 18:33:30 +0900

    feat: Access Denied 및 Unauthorized 예외 처리

--- src/main/java/com/finnect/common/error/CustomExceptionHandler.java
+++ src/main/java/com/finnect/common/error/CustomExceptionHandler.java
@@ -5,12 +5,28 @@ import com.finnect.common.ApiUtils.ApiResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResult<String>> runtimeExceptionHandler(RuntimeException e) {
+        log.error(e.toString());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiUtils.fail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ApiResult<String>> illegalStateExceptionHandler(IllegalStateException e) {
+        log.error(e.toString());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiUtils.fail(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiResult<String>> notFoundExceptionHandler(NotFoundException e) {
@@ -26,17 +42,17 @@ public class CustomExceptionHandler {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiUtils.fail(HttpStatus.FORBIDDEN, e.getMessage()));
     }
 
-    @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<ApiResult<String>> illegalStateExceptionHandler(IllegalStateException e) {
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiResult<String>> accessDeniedExceptionHandler(AccessDeniedException e) {
         log.error(e.toString());
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiUtils.fail(HttpStatus.BAD_REQUEST, e.getMessage()));
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiUtils.fail(HttpStatus.FORBIDDEN, e.getMessage()));
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiResult<String>> runtimeExceptionHandler(RuntimeException e) {
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiResult<String>> authExceptionHandler(AuthenticationException e) {
         log.error(e.toString());
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiUtils.fail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiUtils.fail(HttpStatus.UNAUTHORIZED, e.getMessage()));
     }
 }

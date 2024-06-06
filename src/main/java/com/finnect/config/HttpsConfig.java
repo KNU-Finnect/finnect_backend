ddbe71fa4791e3f3fc4bba4be4099aa2e1451c86
sinswitch <sin6708k@gmail.com>
2024-06-06 17:46:54 +0900

    feat: HTTP to HTTPS 리다이렉트 추가

---
+++ src/main/java/com/finnect/config/HttpsConfig.java
@@ -0,0 +1,41 @@
+package com.finnect.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(8080);
+        connector.setRedirectPort(8443);
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
+}

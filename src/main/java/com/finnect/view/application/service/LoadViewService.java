7c522ed443df09142eb30da4cc14ce8c3da3c4de
sanghee0820 <lsh277604@gmail.com>
2024-06-07 01:54:28 +0900

    refactor: ViewDetail을 리턴하도록 변경

--- src/main/java/com/finnect/view/application/service/LoadViewService.java
+++ src/main/java/com/finnect/view/application/service/LoadViewService.java
@@ -1,9 +1,11 @@
 package com.finnect.view.application.service;
 
+import com.finnect.crm.application.port.out.column.LoadDataColumnPort;
 import com.finnect.view.application.port.in.LoadViewUseCase;
 import com.finnect.view.application.port.out.LoadViewPort;
 import com.finnect.view.domain.Filter;
 import com.finnect.view.domain.View;
+import com.finnect.view.domain.ViewDetail;
 import com.finnect.view.domain.state.ViewState;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +15,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LoadViewService implements LoadViewUseCase {
     private final LoadViewPort loadViewPort;
+    private final LoadDataColumnPort loadDataColumnPort;
     @Override
-    public ViewState loadViewInfo(View view, List<Filter> filters) {
+    public ViewDetail loadViewInfo(View view, List<Filter> filters) {
         view = loadViewPort.loadView(view);
         view.appendFilter(filters);
+        var columns = loadDataColumnPort.loadDataColumnsOfDeal(view.getWorkspaceId());
 
-        return view;
+        return ViewDetail
+                .builder()
+                .view(view)
+                .dataColumns(columns)
+        .build();
     }
 }

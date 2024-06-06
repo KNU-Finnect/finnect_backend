36a93d6f367b8412b1f577d9818cb6fdfa535a80
sanghee0820 <lsh277604@gmail.com>
2024-06-06 18:43:33 +0900

    feat: workspaceId와 ViewType으로 view Load 로직 추가

--- src/main/java/com/finnect/view/adapter/out/ViewRepository.java
+++ src/main/java/com/finnect/view/adapter/out/ViewRepository.java
@@ -1,7 +1,10 @@
 package com.finnect.view.adapter.out;
 
+import com.finnect.crm.domain.column.DataType;
 import com.finnect.view.adapter.out.persistence.ViewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ViewRepository extends JpaRepository<ViewEntity, Long> {
+
+    ViewEntity findViewEntityByWorkspaceIdAndViewType(Long workspaceId, DataType dType);
 }

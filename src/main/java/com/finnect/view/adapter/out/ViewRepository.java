901bbcd484a8a141869702d9b7584fddce51b0cc
sanghee0820 <lsh277604@gmail.com>
2024-06-07 02:16:36 +0900

    feat: WorkspaceId로 DealView 반환 로직 추가

--- src/main/java/com/finnect/view/adapter/out/ViewRepository.java
+++ src/main/java/com/finnect/view/adapter/out/ViewRepository.java
@@ -2,9 +2,12 @@ package com.finnect.view.adapter.out;
 
 import com.finnect.crm.domain.column.DataType;
 import com.finnect.view.adapter.out.persistence.ViewEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ViewRepository extends JpaRepository<ViewEntity, Long> {
 
     ViewEntity findViewEntityByWorkspaceIdAndViewType(Long workspaceId, DataType dType);
+
+    List<ViewEntity> findViewEntitiesByWorkspaceIdAndViewType(Long workspaceId, DataType dType);
 }

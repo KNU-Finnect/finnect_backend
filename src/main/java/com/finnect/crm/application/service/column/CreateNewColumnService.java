ec70bd3da9979b724aec0da16628553dd1cdd709
sanghee0820 <lsh277604@gmail.com>
2024-06-07 18:01:06 +0900

    feat: Default Column 저장 시 Cell 생성
    한번에 저장하도록 수정 예정

--- src/main/java/com/finnect/crm/application/service/column/CreateNewColumnService.java
+++ src/main/java/com/finnect/crm/application/service/column/CreateNewColumnService.java
@@ -35,6 +35,9 @@ public class CreateNewColumnService implements CreateNewColumnUseCase {
     public void createDefaultColumn(Long workspaceId) {
         List<DataColumn> defaultDealColumns = getDefaultColumns(workspaceId);
         defaultDealColumns = saveDataColumnPort.saveColumns(new ArrayList<>(defaultDealColumns));
+        for(DataColumn dataColumn: defaultDealColumns){
+            saveCellPort.saveNewCellByNewColumn(dataColumn);
+        }
         modifyViewUseCase.addViewColumns(new ArrayList<>(defaultDealColumns));
     }
     private List<DataColumn> getDefaultColumns(Long workspaceId){

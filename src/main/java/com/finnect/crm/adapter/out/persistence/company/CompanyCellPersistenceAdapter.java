e5f8a454e5162256cd8ceb05cc78ae1eaaccb2a8
sanghee0820 <lsh277604@gmail.com>
2024-06-07 22:06:56 +0900

    feat: 필터링을 적용한 CompanyCell 반환 로직 추가

---
+++ src/main/java/com/finnect/crm/adapter/out/persistence/company/CompanyCellPersistenceAdapter.java
@@ -0,0 +1,103 @@
+package com.finnect.crm.adapter.out.persistence.company;
+
+import com.finnect.crm.adapter.out.persistence.cell.DataCellEntity;
+import com.finnect.crm.application.port.out.company.LoadCompanyWithCellPort;
+import com.finnect.crm.domain.company.CompanyCell;
+import com.finnect.view.domain.state.FilterState;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class CompanyCellPersistenceAdapter implements LoadCompanyWithCellPort {
+
+    @PersistenceContext
+    private final EntityManager em;
+    private final int BATCH_SIZE = 10;
+
+    @Override
+    public List<CompanyCell> loadCompaniesWithCellsByFilter(List<FilterState> filters, int startPage,
+                                                            int columnCount) {
+        String queryString = generateQuery(filters);
+        log.info(queryString);
+        TypedQuery<Object[]> query = em.createQuery(generateQuery(filters), Object[].class);
+        if(filters != null){
+            setParam(filters, query);
+        }
+        query.setFirstResult(BATCH_SIZE * (startPage - 1));
+        query.setMaxResults(BATCH_SIZE * columnCount);
+        List<Object[]> objects = query.getResultList();
+        for(Object[] object : objects){
+            log.info("object = " + Arrays.toString(object));
+        }
+        return toDomain(objects);
+    }
+
+    private String generateQuery(List<FilterState> filters){
+        log.info("filter : " + filters);
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("SELECT co, c ")
+                .append("FROM company co ")
+                .append("JOIN FETCH data_cell c ON co.dataRowId = c.cellId.dataRowId ");
+
+        int valueIndex = 0;
+        if (filters != null && !filters.isEmpty()) {
+            queryBuilder.append("WHERE c.cellId.dataRowId IN ( ")
+                    .append("SELECT c2.cellId.dataRowId ")
+                    .append("FROM data_cell c2 ")
+                    .append("WHERE ");
+            for (int i = 0; i < filters.size(); i++) {
+                FilterState filter = filters.get(i);
+
+                // 쿼리 조건 추가
+                if (i > 0) {
+                    queryBuilder.append(" AND ");
+                }
+                queryBuilder.append("(c2.cellId.columnId = :columnId")
+                        .append(valueIndex)
+                        .append(" AND ")
+                        .append("c2.value ")
+                        .append(filter.getFilterCondition().getOperator())
+                        .append(" :value")
+                        .append(valueIndex++)
+                        .append(" )");
+            }
+            queryBuilder.append(")");
+        }
+        return queryBuilder.toString();
+    }
+
+    private void setParam(List<FilterState> filters, Query query){
+        String columnBase = "columnId";
+        String valueBase = "value";
+        for(int i = 0; i < filters.size(); i++){
+            FilterState filter = filters.get(i);
+            query.setParameter(columnBase + i, filter.getColumnId());
+            query.setParameter(valueBase + i, filter.getValue());
+        }
+
+    }
+
+    private List<CompanyCell> toDomain(List<Object[]> objects){
+        Map<Long, CompanyCell> companyMap = new HashMap<>();
+        for(Object[] object : objects){
+            CompanyEntity company = (CompanyEntity) object[0];
+            DataCellEntity cell = (DataCellEntity) object[1];
+            companyMap.putIfAbsent(company.getDataRowId(), new CompanyCell(company.getCompanyId(), company.getCompanyName(), company.getDomain()));
+
+            CompanyCell companyCell = companyMap.get(company.getDataRowId());
+            companyCell.addCell(cell);
+        }
+        return companyMap.values().stream().toList();
+    }
+}

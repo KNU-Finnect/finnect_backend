108117db4be92ce9d6a36ba029f143c484cc7782
sanghee0820 <lsh277604@gmail.com>
2024-06-07 22:11:24 +0900

    feat: Company View 조회 API추가

--- src/main/java/com/finnect/view/adapter/in/web/controller/CompanyViewController.java
+++ src/main/java/com/finnect/view/adapter/in/web/controller/CompanyViewController.java
@@ -2,16 +2,20 @@ package com.finnect.view.adapter.in.web.controller;
 
 import com.finnect.common.ApiUtils;
 import com.finnect.common.ApiUtils.ApiResult;
-import com.finnect.crm.application.port.in.cell.LoadDealWithCellUseCase;
+import com.finnect.crm.application.port.in.company.LoadCompanyWithCellUseCase;
+import com.finnect.user.vo.WorkspaceAuthority;
 import com.finnect.view.adapter.in.web.req.FilterRequest;
 import com.finnect.view.adapter.in.web.res.CompanyViewInfoResponse;
 import com.finnect.view.application.port.in.CreateViewUseCase;
 import com.finnect.view.application.port.in.LoadViewUseCase;
+import com.finnect.view.domain.View;
+import com.finnect.view.domain.ViewDetail;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompanyViewController {
     private final CreateViewUseCase createViewUseCase;
     private final LoadViewUseCase loadViewUseCase;
-    private final LoadDealWithCellUseCase loadDealWithCellUseCase;
+    private final LoadCompanyWithCellUseCase loadCompanyWithCellUseCase;
 
 
     @GetMapping("/workspaces/companies/views/{viewId}")
@@ -32,9 +36,29 @@ public class CompanyViewController {
             @RequestParam(required = false) List<FilterRequest> filters,
             @RequestParam(required = true) int page
     ){
+        ViewDetail viewDetail = loadViewUseCase.loadViewInfo(View.builder()
+                        .viewId(viewId)
+                        .workspaceId(
+                                WorkspaceAuthority.from(SecurityContextHolder
+                                        .getContext()
+                                        .getAuthentication()
+                                        .getAuthorities())
+                                .workspaceId().value()
+                        )
+                        .build()
+        );
 
-
-        return new ResponseEntity<>(ApiUtils.success(HttpStatus.OK, new CompanyViewInfoResponse(null, null))
+        var companyCell = loadCompanyWithCellUseCase.loadCompanyWithCell(
+                WorkspaceAuthority.from(SecurityContextHolder
+                                .getContext()
+                                .getAuthentication()
+                                .getAuthorities())
+                        .workspaceId()
+                        .value(),
+                viewDetail.getFilters(),
+                page
+        );
+        return new ResponseEntity<>(ApiUtils.success(HttpStatus.OK, new CompanyViewInfoResponse(viewDetail, companyCell))
                 , HttpStatus.OK);
     }
 }

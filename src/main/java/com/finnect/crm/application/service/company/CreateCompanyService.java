c7917babd5c0a4be906b3e044cc5ee1961340c94
HyeonJeong <wgwjh05169@gmail.com>
2024-06-07 22:33:20 +0900

    fix: company 생성 시 workspace id를 검사하는 조건 추가

--- src/main/java/com/finnect/crm/application/service/company/CreateCompanyService.java
+++ src/main/java/com/finnect/crm/application/service/company/CreateCompanyService.java
@@ -1,5 +1,6 @@
 package com.finnect.crm.application.service.company;
 
+import com.finnect.common.error.CustomException;
 import com.finnect.crm.application.port.in.cell.CreateNewRowUseCase;
 import com.finnect.crm.application.port.in.company.CreateCompanyCommand;
 import com.finnect.crm.application.port.in.company.CreateCompanyUsecase;
@@ -9,7 +10,10 @@ import com.finnect.crm.domain.cell.state.DataRowState;
 import com.finnect.crm.domain.column.DataColumn;
 import com.finnect.crm.domain.company.CompanyState;
 import com.finnect.crm.domain.company.CompanyWithoutId;
+import com.finnect.workspace.application.port.out.CheckQuery;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,11 +25,14 @@ class CreateCompanyService implements CreateCompanyUsecase {
     private final SearchCompanyPort searchCompanyPort;
     private final CreateNewRowUseCase createNewRowUseCase;
     private final SaveCompanyPort saveCompanyPort;
+    private final CheckQuery checkQuery;
 
     @Override
     public CompanyState createCompany(CreateCompanyCommand cmd) {
         if (searchCompanyPort.searchByDomain(cmd.getDomain()))
             throw new RuntimeException(cmd.getDomain() + " 도메인이 이미 존재합니다.");
+        if (checkQuery.checkExistWorkspace(cmd.getWorkspaceId()))
+            throw new CustomException(HttpStatus.BAD_REQUEST, cmd.getWorkspaceId() + " 워크스페이스가 존재하지 않습니다.");
 
         DataRowState dataRow = createNewRowUseCase.createNewDataRow(
                 DataColumn.builder()

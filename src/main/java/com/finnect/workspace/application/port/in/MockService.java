8bcd567bfad2ac2352f1547d602de87f4bcec49d
sinswitch <sin6708k@gmail.com>
2024-06-07 14:45:01 +0900

    feat: 워크스페이스 이동 가능 여부 로직 추가

---
+++ src/main/java/com/finnect/workspace/application/port/in/MockService.java
@@ -0,0 +1,15 @@
+package com.finnect.workspace.application.port.in;
+
+import com.finnect.user.vo.UserId;
+import com.finnect.user.vo.WorkspaceId;
+import org.springframework.stereotype.Service;
+
+// 이 서비스는 임시용으로, 이 서비스가 구현하는 유즈케이스 및 쿼리를 실제로 구현하면 이 서비스를 삭제할 것
+@Service
+public class MockService implements CheckWorkspaceQuery {
+
+    @Override
+    public boolean checkWorkspace(UserId userId, WorkspaceId workspaceId) {
+        return true;
+    }
+}

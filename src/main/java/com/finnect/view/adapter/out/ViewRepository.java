99b1a7883245df7be4354b4b1c5c6ac4802efe8b
sanghee0820 <lsh277604@gmail.com>
2024-06-07 17:00:24 +0900

    feat: Column을 통한 View 반환 추가

--- src/main/java/com/finnect/view/adapter/out/ViewRepository.java
+++ src/main/java/com/finnect/view/adapter/out/ViewRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ViewRepository extends JpaRepository<ViewEntity, Long> {
 
-    ViewEntity findViewEntityByWorkspaceIdAndViewType(Long workspaceId, DataType dType);
-
     List<ViewEntity> findViewEntitiesByWorkspaceIdAndViewType(Long workspaceId, DataType dType);
+    ViewEntity findByWorkspaceIdAndViewType(Long workspaceId, DataType dType);
 }

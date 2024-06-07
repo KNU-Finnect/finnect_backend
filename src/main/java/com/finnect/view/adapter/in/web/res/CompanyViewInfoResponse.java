8b95f3aad7ba1900de9b3ae20974ae832a23ee2b
sanghee0820 <lsh277604@gmail.com>
2024-06-07 22:10:48 +0900

    feat: DTO 생성 로직 추가

--- src/main/java/com/finnect/view/adapter/in/web/res/CompanyViewInfoResponse.java
+++ src/main/java/com/finnect/view/adapter/in/web/res/CompanyViewInfoResponse.java
@@ -1,5 +1,6 @@
 package com.finnect.view.adapter.in.web.res;
 
+import com.finnect.crm.domain.company.CompanyCell;
 import com.finnect.crm.domain.deal.DealCell;
 import com.finnect.view.domain.ViewDetail;
 import java.util.List;
@@ -12,7 +13,7 @@ public class CompanyViewInfoResponse {
     List<FilterResponse> filters;
     List<ViewColumnResponse> viewColumns;
     List<CompanyInfoResponse> viewCompanies;
-    public CompanyViewInfoResponse (ViewDetail viewState, List<DealCell> dealCells) {
+    public CompanyViewInfoResponse (ViewDetail viewState, List<CompanyCell> companyCell) {
         this.viewId = viewState.getViewId();
         this.viewName = viewState.getViewName();
         this.filters = viewState.getFilters().stream()
@@ -31,6 +32,12 @@ public class CompanyViewInfoResponse {
                         .sorting(viewColumn.getSorting())
                         .columnType(viewColumn.getColumnType())
                         .build())
+                .toList();
+        setViewDeals(companyCell);
+    }
+    private void setViewDeals(List<CompanyCell> companyCells){
+        this.viewCompanies = companyCells.stream()
+                .map(CompanyInfoResponse::new)
                 .toList();
     }
 }

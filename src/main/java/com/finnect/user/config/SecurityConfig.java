b1384ce454c09d26967ce403392b771eb4c0fa47
sinswitch <sin6708k@gmail.com>
2024-06-09 02:23:21 +0900

    fix: PasswordConfig로 PasswordEncoder 분리

--- src/main/java/com/finnect/user/config/SecurityConfig.java
+++ src/main/java/com/finnect/user/config/SecurityConfig.java
@@ -95,11 +95,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }

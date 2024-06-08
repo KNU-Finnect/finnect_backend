46b9322373aa0938612db4e8733724de06e2f017
sinswitch <sin6708k@gmail.com>
2024-06-08 23:48:01 +0900

    feat: Workspace ID로 Column 로드 로직 추가

--- src/main/java/com/finnect/crm/application/port/in/column/LoadDataColumnUseCase.java
+++ src/main/java/com/finnect/crm/application/port/in/column/LoadDataColumnUseCase.java
@@ -1,10 +1,13 @@
 package com.finnect.crm.application.port.in.column;
 
 import com.finnect.crm.domain.column.DataColumn;
+import com.finnect.crm.domain.column.DataType;
 import com.finnect.crm.domain.column.state.DataColumnState;
 import java.util.List;
 
 public interface LoadDataColumnUseCase {
+
+    List<DataColumnState> loadDataColumns(Long workspaceId, DataType dType);
 
     List<DataColumnState> loadDataColumns(DataColumn dataColumn);
 }
